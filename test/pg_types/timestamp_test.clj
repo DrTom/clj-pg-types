bbba4de7f528681f0ed57ede94b5c1e588f3dbad
Thomas Schank <DrTom@schank.ch>
2020-05-27 15:37:28 +0200

    This should be it

--- test/pg_types/timestamp_test.clj
+++ test/pg_types/timestamp_test.clj
@@ -6,15 +6,8 @@
     [clojure.java.jdbc :as jdbc]
     [clj-time.local]
     [java-time])
+  (:import [java.sql Timestamp]))
 
-  (:import
-    [java.sql Timestamp]
-    [java.time.temporal ChronoUnit]
-
-    ))
-
-
-(def db-spec (env-db-spec))
 
 (defn create-test-table [tx]
   (jdbc/db-do-commands
@@ -24,27 +17,37 @@
   (->> "select * from test"
        (jdbc/query tx) first :at))
 
-
 (deftest writing-and-reading-timestamps
 
   (testing "writing rand reading a timestamp yields equivalent values "
-    (jdbc/with-db-transaction [tx db-spec]
+    (jdbc/with-db-transaction [tx (env-db-spec)]
       (create-test-table tx)
-      (let [instant (java-time/instant)
-            timestamp (Timestamp/from instant)]
+      (let [timestamp (-> (java-time/instant)
+                          java-time/instant->sql-timestamp)]
         (jdbc/insert! tx :test {:at timestamp})
         (let [at (first-at-in-test tx)]
-          (is (= java.sql.Timestamp (type at)))
+          (is (= Timestamp (type at)))
           (is (= timestamp at))))))
 
   (testing "we can write iso8601 and retrieve java.sql.timestamp
            WITH RESTRICTED PRECICION to milliseconds"
-    (jdbc/with-db-transaction [tx db-spec]
+    (jdbc/with-db-transaction [tx (env-db-spec)]
       (jdbc/db-do-commands tx "CREATE TEMP TABLE test (at timestamp WITH TIME ZONE )")
-      (let [instant (java-time/truncate-to (java-time/instant) :millis)
+      (let [instant (-> (java-time/instant)
+                        (java-time/truncate-to :millis))
             iso8601 (str instant)
             timestamp (Timestamp/from instant)]
         (jdbc/insert! tx :test {:at iso8601})
         (let [at (first-at-in-test tx)]
-          (is (= java.sql.Timestamp (type at)))
+          (is (= Timestamp (type at)))
+          (is (= timestamp at))))))
+
+  (testing "we can still write joda retrieve java.sql.timestamp"
+    (jdbc/with-db-transaction [tx (env-db-spec)]
+      (jdbc/db-do-commands tx "CREATE TEMP TABLE test (at timestamp WITH TIME ZONE )")
+      (let [joda-date-time (clj-time.core/now)
+            timestamp (clj-time.coerce/to-sql-time (clj-time.core/now))]
+        (jdbc/insert! tx :test {:at joda-date-time})
+        (let [at (first-at-in-test tx)]
+          (is (= Timestamp (type at)))
           (is (= timestamp at)))))))
