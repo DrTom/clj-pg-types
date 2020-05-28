8c24e4308b2b6a1f1af5344c2ce3d264f3563d67
Thomas Schank <DrTom@schank.ch>
2020-05-28 14:54:18 +0200

    Rewrite uuid test

--- test/pg_types/uuid_test.clj
+++ test/pg_types/uuid_test.clj
@@ -1,35 +1,34 @@
 (ns pg-types.uuid-test
   (:require
-    [midje.sweet :refer :all]
+    [pg-types.all]
     [pg-types.connection :refer :all]
-    [pg-types.all :refer :all]
     [clojure.java.jdbc :as jdbc]
-    [clj-time.core :as time-core]
-    ))
+    [clojure.test :refer :all]))
 
 (def db-spec (env-db-spec))
 
 
-(facts "uuid"
-       (jdbc/with-db-transaction [tx db-spec]
-         (jdbc/db-do-commands tx "CREATE TEMP TABLE test (id uuid)")
-         (let [str-uuid "65b85773-c935-439f-a3c0-6c538f84825d"
-               uuid (java.util.UUID/fromString str-uuid) ]
+(deftest uuid
+  (jdbc/with-db-transaction [tx db-spec]
+    (jdbc/db-do-commands tx "CREATE TEMP TABLE test (id uuid)")
+    (let [str-uuid "65b85773-c935-439f-a3c0-6c538f84825d"
+          uuid (java.util.UUID/fromString str-uuid) ]
 
-           (facts "result of inserting a string value"
-                  (let [result-value (:id (first (jdbc/insert! tx :test {:id str-uuid})))]
-                    (fact "is the equivalent uuid type" result-value => uuid)))
+      (testing "result of inserting a string value"
+        (let [result-value (:id (first (jdbc/insert! tx :test {:id str-uuid})))]
+          (testing "is the equivalent uuid type" (is (= result-value uuid)))))
 
-           (facts "result of querying with a string value"
-                  (let [res (first  (jdbc/query tx ["SELECT * FROM test WHERE id = ?" str-uuid]))]
-                    (fact "yields the equivalent uuid type" res => {:id uuid})))
-           (facts "result of querying with a uuid value"
-                  (let [res (first  (jdbc/query tx ["SELECT * FROM test WHERE id = ?" uuid]))]
-                    (fact "yields the equivalent uuid type" res => {:id uuid})))
-           )
-         (let [ruuid (java.util.UUID/randomUUID)]
-           (facts "result of inserting a random uuid of type UUID"
-                  (let [result-value (:id (first (jdbc/insert! tx :test {:id ruuid})))]
-                    (fact "is the equivalent uuid type" result-value => ruuid))))))
+      (testing "result of querying with a string value"
+        (let [res (first  (jdbc/query tx ["SELECT * FROM test WHERE id = ?" str-uuid]))]
+          (testing "yields the equivalent uuid type" (is (= res {:id uuid})))))
+
+      (testing "result of querying with a uuid value"
+        (let [res (first  (jdbc/query tx ["SELECT * FROM test WHERE id = ?" uuid]))]
+          (testing "yields the equivalent uuid type" (is (=  res {:id uuid}))))))
+
+    (let [ruuid (java.util.UUID/randomUUID)]
+      (testing "result of inserting a random uuid of type UUID"
+        (let [result-value (:id (first (jdbc/insert! tx :test {:id ruuid})))]
+          (testing "is the equivalent uuid type" (is (= result-value ruuid))))))))
 
 
