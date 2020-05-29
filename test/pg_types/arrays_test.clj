0e24e9e4f62add88695e4e1c65cc9de49d0f41a1
Thomas Schank <DrTom@schank.ch>
2020-05-29 08:26:55 +0200

    Test array tests

--- test/pg_types/arrays_test.clj
+++ test/pg_types/arrays_test.clj
@@ -1,43 +1,45 @@
 (ns pg-types.arrays-test
   (:require
-    [midje.sweet :refer :all]
     [pg-types.connection :refer :all]
-    [pg-types.all :refer :all]
+    [pg-types.all]
     [clojure.java.jdbc :as jdbc]
-    [clj-time.core :as time-core]
-    [clojure.tools.logging :as logging]
-    ))
+    [clojure.test :refer :all]
+    [java-time]))
 
 (def db-spec (env-db-spec))
 
-(fact "arrays"
-      (jdbc/with-db-transaction [tx db-spec]
-        (jdbc/db-do-commands tx "CREATE TEMP TABLE test (iarray integer[], tarray text[])")
-        (facts "result of inserting "
-               (let [ result (first (jdbc/insert! tx :test {:iarray  [1 2 3]
-                                                            :tarray ["Foo" "Bar" "Baz"]}))]
-                 (fact "vector of integers is equal to input" (:iarray result) => [1 2 3])
-                 (fact "vector of strings is equal to input" (:tarray result) => ["Foo" "Bar" "Baz"])
-                 ))))
+(deftest arrays
+  (jdbc/with-db-transaction [tx db-spec]
+    (jdbc/db-do-commands tx "CREATE TEMP TABLE test
+                            (iarray integer[], tarray text[])")
+    (testing "result of inserting "
+      (let [result (->> {:iarray  [1 2 3]
+                         :tarray ["Foo" "Bar" "Baz"]}
+                        (jdbc/insert! tx :test ) first)]
+        (testing "vector of integers is equal to input"
+          (is (= (:iarray result) [1 2 3])))
+        (testing "vector of strings is equal to input"
+          (is (= (:tarray result) ["Foo" "Bar" "Baz"])))))))
 
-(fact "lazy seq"
-      (jdbc/with-db-transaction [tx db-spec]
-        (jdbc/db-do-commands tx "CREATE TEMP TABLE test (iarray integer[])")
-        (facts "result of inserting "
-               (let [result (first (jdbc/insert! tx :test {:iarray  (map identity [1 2 3])}))]
-                 (fact "equal to input" (:iarray result) => [1 2 3])
-                 ))))
+(deftest lazy-seqs
+  (jdbc/with-db-transaction [tx db-spec]
+    (jdbc/db-do-commands tx "CREATE TEMP TABLE test (iarray integer[])")
+    (testing "result of inserting "
+      (let [result (->> {:iarray  (map identity [1 2 3])}
+                        (jdbc/insert! tx :test) first)]
+        (testing "equal to input"
+          (is (= (:iarray result) [1 2 3])))))))
 
 
-(fact "array of timestamp converted"
-      (jdbc/with-db-transaction [tx db-spec]
-        (jdbc/db-do-commands tx "SET LOCAL TIME ZONE 'UTC'")
-        (jdbc/db-do-commands tx "CREATE TEMP TABLE test (tarray timestamp WITH TIME ZONE[])")
-        (let [now (time-core/now)
-              now-iso8601-str (str now) ]
-          ;(logging/warn now-iso8601-str)
-          (facts "result of inserting "
-                 (let [result (first (jdbc/insert! tx :test {:tarray [now-iso8601-str]}))]
-                   (fact "equal to input" (:tarray result) => [now])
-                   )))))
+(deftest array-of-timestamps
+  (jdbc/with-db-transaction [tx db-spec]
+    (jdbc/db-do-commands tx "CREATE TEMP TABLE test
+                            (tarray timestamp WITH TIME ZONE[])")
+    (let [timestamp (-> (java-time/instant)
+                        java-time/instant->sql-timestamp)]
+      (testing "result of inserting and array of timestamps"
+        (let [result (->> {:tarray [timestamp]}
+                          (jdbc/insert! tx :test) first)]
+          (testing "is equal to input"
+                  (is (= (:tarray result) [timestamp]))))))))
 
