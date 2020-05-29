8bd0c09e0343575dff0252fe3778cf26d6238fda
Thomas Schank <DrTom@schank.ch>
2020-05-29 08:14:15 +0200

    convert char test

--- test/pg_types/char_test.clj
+++ test/pg_types/char_test.clj
@@ -1,22 +1,23 @@
 (ns pg-types.char-test
   (:require
-    [midje.sweet :refer :all]
     [pg-types.connection :refer :all]
-    [pg-types.all :refer :all]
+    [pg-types.all]
     [clojure.java.jdbc :as jdbc]
-    [clj-time.core :as time-core]
+    [clojure.test :refer :all]
     ))
 
 (def db-spec (env-db-spec))
 
-
-
-; better test this since we implement java.lang.string
-(facts "character types"
-       (let [insert-map {:varchar_field  "Foo" :char_field "0123456789" :text_field "Bar"} ]
-         (jdbc/with-db-transaction [tx db-spec]
-           (jdbc/db-do-commands tx "CREATE TEMP TABLE test (varchar_field varchar, char_field char(10), text_field text)")
-           (let [inserted-row (first (jdbc/insert! tx :test insert-map))]
-             (fact inserted-row => insert-map)))))
+(deftest char-types
+  (testing "character types"
+    (let [insert-map {:varchar_field  "Foo"
+                      :char_field "0123456789"
+                      :text_field "Bar"}]
+      (jdbc/with-db-transaction [tx db-spec]
+        (jdbc/db-do-commands
+          tx "CREATE TEMP TABLE test
+             (varchar_field varchar, char_field char(10), text_field text)")
+        (let [inserted-row (first (jdbc/insert! tx :test insert-map))]
+          (is (= inserted-row insert-map)))))))
 
 
